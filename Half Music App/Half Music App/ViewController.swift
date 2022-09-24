31eef309d0870ef20acf13153fa9a896d8741aa2
VasckMe <kasarin.anton@gmail.com>
2022-09-24 14:11:11 +0300

    Project created

---
+++ Half Music App/Half Music App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Half Music App
+//
+//  Created by Apple Macbook Pro 13 on 22.09.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
